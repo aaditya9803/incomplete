e1c33815c78f4773318ae7f3b05b03fb212f87e8
aaditya9803 <aaditya9803@gmail.com>
2023-01-09 23:48:13 +0545

    deleted some files

--- webscrapper0.py
+++
@@ -1,8 +0,0 @@
-from bs4 import *
-
-with open("beautiful soup\website.html", encoding="utf-8") as file:
-    contents = file.read()
-    
-soup = BeautifulSoup(contents,"html.parser")
-all_a_tags = soup.find_all(name="a")
-print(all_a_tags)
